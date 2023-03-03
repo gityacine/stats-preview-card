34794383b0cfe3c74ccef522e422f4451b6f1ed8
Yacine Haddouche <100038941+gityacine@users.noreply.github.com>
2023-03-03 11:52:43 +0100

    made the card responsive

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,18 +3,18 @@ import stats from "../data";
 
 export default function Card() {
     return (
-        <div className="flex flex-col bg-[#1b1938] rounded-xl overflow-hidden m-6">
-            <div className="relative">
+        <div className="flex flex-col sm:flex-row sm:w-[58rem] bg-[#1b1938] rounded-xl shadow-lg overflow-hidden m-6">
+            <div className="relative sm:basis-1/2 sm:order-last">
                 <picture>
                     <source media="(min-width: 640px)" srcSet="image-header-desktop.jpg" />
-                    <img src="image-header-mobile.jpg" alt="background image" />
+                    <img className="sm:h-full sm:w-full sm:object-cover" src="image-header-mobile.jpg" alt="background image" />
                 </picture>
                 <div className="absolute top-0 w-full h-full bg-[#aa5cdb]/50"></div>
             </div>
-            <div className="flex flex-col py-9 px-10">
-                <h1 className="font-inter font-bold text-3xl text-white text-center">Get <span className="text-[#aa5cdb]">insights</span> that help your business grow.</h1>
-                <p className="font-inter font-normal text-base leading-7 text-white/75 text-center mt-5">Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</p>
-                <div className="flex flex-col space-y-8 mt-10">
+            <div className="flex flex-col sm:basis-1/2 sm:justify-center py-9 px-10">
+                <h1 className="font-inter font-bold text-3xl text-white text-center sm:text-left">Get <span className="text-[#aa5cdb]">insights</span> that help your business grow.</h1>
+                <p className="font-inter font-normal text-base leading-7 text-white/75 text-center sm:text-left mt-5">Discover the benefits of data analytics and make better decisions regarding revenue, customer experience, and overall efficiency.</p>
+                <div className="flex flex-col sm:flex-row space-y-8 sm:space-y-0 sm:space-x-10 mt-10">
                     {stats.map((item, index) => (
                         <Stat
                             key={index}
--- src/components/Stat.jsx
+++ src/components/Stat.jsx
@@ -1,7 +1,7 @@
 
 export default function Stat(props) {
     return (
-        <div className="flex flex-col items-center">
+        <div className="flex flex-col items-center sm:items-start">
             <h2 className="font-inter font-bold text-[1.7rem] text-white">{props.value}</h2>
             <span className="font-lexenddeca uppercase text-[15px] text-white/60 mt-1">{props.cat}</span>
         </div>
